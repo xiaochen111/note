0834d37ebb2762eef4d55dec56c3a27dd017c3e2
Zhikuo.Fan <zhikuo.fan@mail.haoyisheng.com>
2017-06-08 09:11:21 +0800

    readline and real knowledge

---
+++ notes/readline.js
@@ -0,0 +1,21 @@
+const readline = require('readline');
+const question = ['what is your name ? > ', 'how old are you ? > '];
+const index = 0;
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: question[0]
+})
+rl.prompt();
+rl.on('line', (data) => {
+    console.log('hello ' + data);
+    index += 1;
+    if(question[index]) {
+        rl.setPrompt(question[index]);
+        rl.prompt();
+    } else {
+        rl.close();
+    }
+}).on('close', () => {
+    console.log('再见！');
+});
